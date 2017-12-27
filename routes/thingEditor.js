871cfbe2be100828c8b4f636bfe723998c167457
Filip Kiraa Opálený <wire150@gmail.com>
2017-12-27 19:30:33 +0100

    file upload test

--- routes/thingEditor.js
+++ routes/thingEditor.js
@@ -3,10 +3,24 @@ var fs = require('fs');
 var router = express.Router();
 
 var files = fs.readdirSync('./');
+function createNewFile(filename) {
+    fs.writeFileSync(filename, 'lisk');
+}
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.render('thingEditor', { title: "vagina", files: files });
+    next();
+});
+
+
+router.post('/', function(req, res, next) {
+    var name = req.body.newFileName;
+    createNewFile(name);
+    res.render('thingEditor', { title: name, files: files });
+    next();
+
 });
 
 module.exports = router;
